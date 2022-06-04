93da5a2ba403cbca4cda47c4fa5cb7acf8d08434
Mahdi Parivash <mahdiparivash@protonmail.com>
2022-06-04 22:32:11 +0430

    first day

---
+++ README.md
@@ -0,0 +1,3 @@
+# 100-days-js-project
+
+100 days of coding chalnge with mini javascript project
---
+++ day-1-downloader/index.js
@@ -0,0 +1,28 @@
+const fileInput = document.querySelector("input");
+const downloadButton = document.querySelector("button");
+
+downloadButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  downloadButton.innerText = "Downloading file ...";
+  fetchFile(fileInput.value);
+});
+
+function fetchFile(url) {
+  fetch(url)
+    .then((res) => res.blob())
+    .then((file) => {
+      let tempUrl = URL.createObjectURL(file);
+      let aTag = document.createElement("a");
+      aTag.href = tempUrl;
+      aTag.download = "filename";
+      document.body.appendChild(aTag);
+      aTag.click();
+      aTag.remove();
+      URL.revokeObjectURL(tempUrl);
+      downloadButton.innerText = "Download File";
+    })
+    .catch(() => {
+      downloadButton.innerText = "Download file";
+      alert("Failed to download file");
+    });
+}
